81666173673f22fa96a3a8e1ea84b2227edf9183
Amir Arsalan Soltani <Amir-Arsalan@users.noreply.github.com>
2016-08-17 18:47:20 -0400

    Create subnetworkBuilder.lua

---
+++ subnetworkBuilder.lua
@@ -0,0 +1,20 @@
+require 'torch'
+require 'nn'
+
+local subnetworkBuilder = {}
+
+function subnetworkBuilder.get_generator()
+  local generator = nn.Sequential()
+  -- Add layers of your own interest
+  
+  return generator
+end
+
+function subnetworkBuilder.get_discriminator()
+  local generator = nn.Sequential()
+  -- Add layers of your own interest
+  
+  return discriminator
+end
+
+return subnetworkBuilder
